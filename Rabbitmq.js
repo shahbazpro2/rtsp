762b06f71c42c1e2f3c6e9278cf9982601f156fb
Muhammad <shahbaz.pro2@gmail.com>
2024-09-06 21:53:17 +0500

    Uncomment socket

--- Rabbitmq.js
+++ Rabbitmq.js
@@ -24,7 +24,7 @@ export async function Rabbitmq(currentQueueData, socket) {
   try {
     console.log("reached");
     /*     const connection = await amqp.connect("amqp://NVR:NVR@localhost:5672"); */
-    socket.emit("NVR-Alert", { "Camera 14": false, "camera x": false, "Camera 15": true, "Camera 16": false, "Camera 5": false, "Camera 9": false, "Camera 11": false, "Camera 12": false });
+    /* socket.emit("NVR-Alert", { "Camera 14": false, "camera x": false, "Camera 15": true, "Camera 16": false, "Camera 5": false, "Camera 9": false, "Camera 11": false, "Camera 12": false });
     socket.emit("Camera-Status", {
       "Camera 5": "rtsp://admin:12345678k@10.10.10.220",
       "Camera 9": "rtsp://admin:12345678k@10.10.10.221",
@@ -34,13 +34,13 @@ export async function Rabbitmq(currentQueueData, socket) {
       "camera x": "rtsp://admin:12345678k@10.10.10.235:554/Streaming/Channels/102",
       "Camera 15": "rtsp://admin:12345678k@10.10.10.219",
       "Camera 16": "rtsp://admin:$$$DoNotEnter$$$@10.10.10.241",
-    });
-    /*   const connection = await amqp.connect("amqp://127.0.0.1:5672");
+    }); */
+    const connection = await amqp.connect("amqp://127.0.0.1:5672");
     const channel = await connection.createChannel();
     socket.emit(queue1, currentQueueData[queue1]);
     socket.emit(queue2, currentQueueData[queue2]);
     queueFetch(channel, socket, currentQueueData, queue1);
-    queueFetch(channel, socket, currentQueueData, queue2); */
+    queueFetch(channel, socket, currentQueueData, queue2);
 
     /*  console.log("Waiting for messages in", queue); */
   } catch (error) {
