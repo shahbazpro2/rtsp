38de337612c2ee5fd8cf6ca3826e5831026c08e8
Muhammad <shahbaz.pro2@gmail.com>
2024-09-11 03:23:43 +0500

    Change loader color

--- components/ui/Loader.jsx
+++ components/ui/Loader.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div>
-            <LoaderCircle className='animate-spin size-10' />
+            <LoaderCircle className='animate-spin size-10 text-green-500' />
         </div>
     )
 }
