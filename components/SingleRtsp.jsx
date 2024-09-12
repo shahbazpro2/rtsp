6f425d7704c84ecc37a4a6d406ccc2ce60d9d1ec
Muhammad <shahbaz.pro2@gmail.com>
2024-09-13 03:11:11 +0500

    add loader

--- components/SingleRtsp.jsx
+++ components/SingleRtsp.jsx
@@ -1,7 +1,9 @@
 import JSMpeg from '@cycjimmy/jsmpeg-player';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Loader from './ui/Loader';
 
 const SingleRtsp = ({ id }) => {
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         if (!id) return;
@@ -13,11 +15,25 @@ const SingleRtsp = ({ id }) => {
         const player = new JSMpeg.VideoElement("#video-canvas", videoUrl, {
             autoplay: true,
         });
-        console.log(player);
+        const interval = setInterval(() => {
+            if (player?.player?.currentTime > 0) {
+                setLoading(false); // Hide loader when the video starts playing
+                clearInterval(interval); // Stop polling
+            }
+        }, 500); // Poll every 500ms
+
+        return () => {
+            clearInterval(interval);
+        }
     }, [id]);
 
     return (
-        <div>
+        <div className='relative'>
+            {loading && (
+                <div className="absolute inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-[20000]">
+                    <Loader />
+                </div>
+            )}
             <div id="video-canvas" style={{ height: 565, width: 1000 }}></div>
         </div>
     )
