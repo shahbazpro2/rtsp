38020488d49dee63f18b64b16891c9ad40f4db94
Muhammad <shahbaz.pro2@gmail.com>
2024-10-29 11:27:23 +0500

    Fix import issue

--- hooks/useUserSettings.js
+++ hooks/useUserSettings.js
@@ -1,4 +1,5 @@
 import { getUserSettings } from '@/apis/settings'
+import { useApi } from 'use-hook-api'
 
 const useUserSettings = () => {
     const [, res] = useApi({ cache: 'userSettings' }, getUserSettings())
